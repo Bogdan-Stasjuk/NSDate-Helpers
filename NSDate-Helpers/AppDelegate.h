a83babeb12f7097132fd2d4ac4a9720fea627dc2
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-04-08 23:14:46 +0300

    Initial Commit

---
+++ NSDate-Helpers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSDate-Helpers
+//
+//  Created by Bogdan Stasjuk on 4/8/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSDate-Helpers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSDate-Helpers
+//
+//  Created by Bogdan Stasjuk on 4/8/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
